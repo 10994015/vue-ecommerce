566e1b562e08651a30f9d6a0224cb7ce5dc8fbf2
10994015 <a0938599191@gmail.com>
2023-03-09 06:00:02 +0800

    not auth go to login page

--- backend/src/components/AppLayout.vue
+++ backend/src/components/AppLayout.vue
@@ -1,16 +1,35 @@
 <script setup>
-    const {title} = defineProps({
-        title:String,
-    });
+
+  import { ref, onMounted, onUnmounted } from "vue";
+  import Sidebar from "../components/Sidebar.vue"
+  import Navbar from "./Navbar.vue"
+  const {title} = defineProps({
+      title:String,
+  });
+  const handleSidebarOpened = ()=>{
+    sidebarOpened.value =  window.outerWidth > 768;
+  }
+  onMounted(() => {
+    handleSidebarOpened();
+    window.addEventListener('resize', handleSidebarOpened);
+  })
+  onUnmounted(() => {
+    window.removeEventListener('resize', handleSidebarOpened);
+  })
+  const sidebarOpened = ref(true);
+  const toggleSidebar = ()=>{
+    sidebarOpened.value = !sidebarOpened.value;
+  };
 </script>
 
 <template>
-<div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div class="w-full max-w-md space-y-8">
-        <div>
-          Part of the layout
-        </div>
-        <router-view></router-view>
+    <div class="flex min-h-full bg-gray-100">
+      <Sidebar :class="{'-ml-[200px]':!sidebarOpened}" />
+      <div class="flex-1">
+        <Navbar @toggle-sidebar="toggleSidebar" />
+        <main class="p-6">
+          <router-view></router-view>
+        </main>
       </div>
     </div>
 </template>
